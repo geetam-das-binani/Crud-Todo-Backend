18f659c29aa47ce419a60786fe339371fb28f275
Geetam <geetambinani6@gmail.com>
2023-08-08 20:16:48 +0530

    fixed some errors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 connect()
-app.use(express.json())
+app.use(express.json()) 
 app.use(express.urlencoded({extended:true}))
 app.use('/',router)
 app.listen(8000, () => console.log("Server is listening on port 8000"));
