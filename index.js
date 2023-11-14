d140194cc3f6d184d203d1310eb3982bb0311941
Geetam <geetambinani6@gmail.com>
2023-11-14 11:13:16 +0530

    add env port

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { connect } from "./databse/connect.mjs";
 
  
 dotenv.config();
+const PORT=process.env.PORT || 8001
 
 const app = express();
 app.use(cors());
@@ -15,5 +16,5 @@ connect(process.env.DB_PASS, process.env.DB_NAME);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
-app.listen(8000, () => console.log("Server is listening on port 8000"));
+app.listen(PORT, () => console.log("Server is listening on port 8000"));
  
